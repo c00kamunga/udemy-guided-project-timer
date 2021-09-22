2602e533c8a1c33fa51e0fbdf567a32c5223cadc
Barinder <tsothalanti2@yahoo.com>
2021-09-21 19:48:47 -0700

    added value property to duration in index html and updated tick method in index js file.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-class Timer{
-    constructor(durationInput, startButton, pauseButton){
-this.durationInput = durationInput;
-this.startButton = startButton;
-this.pauseButton = pauseButton;
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
 
-this.startButton.addEventListener('click', this.start)
-this.pauseButton.addEventListener('click', this.pause)
-    }
-    start = () => {
-        this.tick()
-this.interval = setInterval(this.tick, 1000)
-    }
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
 
-    pause = () => {
-clearInterval(this.interval)
-    }
+  pause = () => {
+    clearInterval(this.interval);
+  };
 
-    tick = () => {
-        console.log('tick')
-    }
-
+  tick = () => {
+    const timeRemaining = parseFloat(this.durationInput.value);
+    this.durationInput.value = timeRemaining - 1;
+  };
 }
 
-const durationInput = document.querySelector('#duration')
-const startButton = document.querySelector('#start')
-const pauseButton = document.querySelector('#pause')
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton);
