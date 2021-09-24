5a1b27985f6781857060a36de5c4c577e757ac39
Barinder <tsothalanti2@yahoo.com>
2021-09-23 19:25:32 -0700

    removed timer class from index.js file and transfered to timer.js, added script tag for timer.js

--- index.js
+++ index.js
@@ -1,65 +1,15 @@
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callBacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-    if (callBacks){
-    this.onStart = callBacks.onStart
-    this.onTick = callBacks.onTick
-    this.onComplete = callBacks.onComplete
-    }
-
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-  start = () => {
-    if(this.onStart){
-      this.onStart()
-    }
-
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-
-  tick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-      if(this.onComplete){
-        this.onComplete()
-      }
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-      if(this.onTick){
-        this.onTick()
-      }
-    }
-  };
-
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart(){
-console.log('Timer started')
+  onStart() {
+    console.log('Timer started');
   },
-  onTick(){
-console.log('Timer just ticked down')
+  onTick() {
+    console.log('Timer just ticked down');
   },
-  onComplete(){
-    console.log('Timer is completed')
-  }
+  onComplete() {
+    console.log('Timer is completed');
+  },
 });
